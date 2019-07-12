8ba79be8b5e649d4799a00be5d6480bd0a9d5e5f
Jake Napper <napperjlg@gmail.com>
2019-07-12 13:35:05 +0100

    + and - buttons change the life total

--- src/components/player.js
+++ src/components/player.js
@@ -4,12 +4,23 @@ class Player extends Component {
   state = {
     lifeTotal: 20
   };
+
+  increaseLifeTotal = () => {
+    this.setState({ lifeTotal: this.state.lifeTotal + 1 });
+  };
+
+  decreaseLifeTotal = () => {
+    this.setState({ lifeTotal: this.state.lifeTotal - 1 });
+  };
+
   render() {
     return (
       <div>
         <div>
           <h1>{this.state.lifeTotal}</h1>
         </div>
+        <button onClick={this.increaseLifeTotal}>+</button>
+        <button onClick={this.decreaseLifeTotal}>-</button>
       </div>
     );
   }
