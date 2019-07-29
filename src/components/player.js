78ebb03e5c9d7c952c877574043c85413bc7e0d7
Jake Napper <napperjlg@gmail.com>
2019-07-29 13:58:29 +0100

    refactored playerLifeTotal into seperate component

--- src/components/player.js
+++ src/components/player.js
@@ -1,28 +1,18 @@
 import React, { Component } from "react";
 import PlayerName from "./playerName";
-
+import PlayerLifeTotal from "./playerLifeTotal";
 class Player extends Component {
-  state = {
-    lifeTotal: 20
-  };
-
-  increaseLifeTotal = () => {
-    this.setState({ lifeTotal: this.state.lifeTotal + 1 });
-  };
-
-  decreaseLifeTotal = () => {
-    this.setState({ lifeTotal: this.state.lifeTotal - 1 });
-  };
+  state = {};
 
   render() {
     return (
       <div>
         <div>
           <PlayerName />
-          <h1>{this.state.lifeTotal}</h1>
         </div>
-        <button onClick={this.increaseLifeTotal}>+</button>
-        <button onClick={this.decreaseLifeTotal}>-</button>
+        <div>
+          <PlayerLifeTotal />
+        </div>
       </div>
     );
   }
